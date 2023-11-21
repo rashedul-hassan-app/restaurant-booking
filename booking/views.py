b10ac6f497fa5f8a6070ff110cd4e25257bef92d
Rashedul Hassan <rashedul@outlook.com>
2023-11-21 23:30:32 +0000

    Do not show booked slots in booking page

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,5 @@
+from .models import BookingDate, TimeSlot, Booking
+from django.shortcuts import render, redirect, get_object_or_404
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import BookingDate, Booking, TimeSlot
 from .forms import TimeSlotForm, BookingForm
@@ -13,16 +15,28 @@ def view_all(request):
     return render(request, 'booking/view_all.html', {'bookings': user_bookings})
 
 
+@login_required
 def create_booking(request):
+    # Get all dates and their available time slots
+    dates = BookingDate.objects.filter(date__gte=date.today())
+    booked_slots = set(TimeSlot.objects.filter(
+        booking__isnull=False).values_list('id', flat=True))
+
     if request.method == 'POST':
         timeslot_id = request.POST.get('booking_slot')
-        timeslot = TimeSlot.objects.get(id=timeslot_id)
+        timeslot = get_object_or_404(TimeSlot, id=timeslot_id)
+
+        # Check if the timeslot is already booked
+        if timeslot.id in booked_slots:
+            messages.error(request, 'This timeslot is already booked.')
+            return render(request, 'booking/create.html', {'dates': dates, 'booked_slots': booked_slots})
+
+        # Create the booking
         Booking.objects.create(user=request.user, timeslot=timeslot)
         messages.success(request, 'Booking created successfully!')
         return redirect('booking_page')
 
-    dates = BookingDate.objects.all().prefetch_related('time_slots')
-    return render(request, 'booking/create.html', {'dates': dates})
+    return render(request, 'booking/create.html', {'dates': dates, 'booked_slots': booked_slots})
 
 
 def edit_booking(request, booking_id):
