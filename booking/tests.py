5ce0e23b5e42d536b7aac7b72c143f38d3100227
Rashedul Hassan <rashedul@outlook.com>
2024-02-06 18:18:45 +0000

    Fine tune bits

--- booking/tests.py
+++ booking/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .forms import BookingForm
 
-# Create your tests here.
+
+class BookingFormTest(TestCase):
+
+    def test_booking_form_date_field_label(self):
+        form = BookingForm()
+        self.assertTrue(
+            form.fields['timeslot'].label is None or form.fields['timeslot'].label == 'timeslot')
